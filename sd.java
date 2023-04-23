4666126af8010cdc041431de320679bed3fd3498
Madhav <123637433+LNSIKHCSM@users.noreply.github.com>
2023-04-23 13:37:34 +0530

    Add files via upload

--- sd.java
+++ sd.java
@@ -20,26 +20,20 @@ public class sd {
         frame.setVisible(true);
         
         
-        String[] regions = {"INDIA", "EUROPE", "ASIA", "AUSTRALIA"};
-        int currentRegionIndex = 0;
-        String currentRegion = regions[currentRegionIndex];
+      
         
         while (true) {
             LocalTime time = LocalTime.now();
             LocalDate date = LocalDate.now();
-            String formattedTime = time.format(DateTimeFormatter.ofPattern("HH:mm:ss"));
+            String formattedTime = time.format(DateTimeFormatter.ofPattern("hh:mm:ss a"));
             String formattedWeekday = date.format(DateTimeFormatter.ofPattern("EEEE"));
-            String formattedMonth = date.format(DateTimeFormatter.ofPattern("MMMM"));
-            String formattedYear = date.format(DateTimeFormatter.ofPattern("yyyy"));
-            panel.setTime(formattedMonth, formattedWeekday, formattedYear, formattedTime, currentRegion);
+            String formattedDate = date.format(DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+            panel.setTime(formattedDate, formattedWeekday, formattedTime);
             TimeUnit.SECONDS.sleep(1);
             panel.repaint();
             
             
-            if (time.getMinute() == 0 && time.getSecond() == 0) {
-                currentRegionIndex = (currentRegionIndex + 1) % regions.length;
-                currentRegion = regions[currentRegionIndex];
-            }
+           
         }
     }
 }
@@ -47,9 +41,8 @@ public class sd {
 class ClockPanel extends JPanel {
     private String time;
     private String weekday;
-    private String month;
-    private String year;
-    private String region;
+    private String date;
+    
 
     public ClockPanel() {
         setPreferredSize(new Dimension(400, 190));
@@ -58,28 +51,28 @@ class ClockPanel extends JPanel {
         setForeground(Color.GREEN);
     }
 
-    public void setTime(String weekday, String month, String year, String time, String region) {
-        this.weekday = weekday;
-        this.month = month;
-        this.year = year;
+    public void setTime(String weekday, String date, String time) {   
+       
+        this.weekday = "Todays date is " + weekday;
+        this.date = "Today's day is: " + date;
         this.time = time;
-        this.region = region;
+        
     }
 
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
+        g.setColor(Color.RED);
+        g.setFont(new Font("Arial", Font.BOLD, 24));
+        g.drawString(date, 50, 50);
+        g.drawString(time, 50, 80);
+        g.drawString(weekday, 50, 110);
         g.setColor(Color.GREEN);
-        g.setFont(new Font("Arial", Font.BOLD, 32));
-        g.drawString(weekday, 50, 50);
-        g.drawString(month, 50, 90);
-        g.drawString(year, 50, 130);
-        g.drawString(time, 200, 90);
-        g.setColor(Color.WHITE);
         g.drawLine(0, 0, getWidth(), 0);
         g.drawLine(0, 0, 0, getHeight());
         g.drawLine(0, getHeight() - 1, getWidth(), getHeight() - 1);
         g.drawLine(getWidth() - 1, 0, getWidth() - 1, getHeight());
-        g.drawString(region, getWidth() - 150, 50);
+        g.setFont(new Font("Arial", Font.BOLD, 18));
+       
     }
 }
